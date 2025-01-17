3dcbe96874b4618477cda4fda3623fe30a4bb633
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2025-01-17 16:28:54 -0300

    first commit

---
+++ R-introduction/operations.R
@@ -0,0 +1,32 @@
+# integer declaration
+delta = 8
+class(delta)
+
+# another types
+delta = 8L
+class(delta)
+
+boolean = TRUE
+class(boolean)
+
+booleanFalse = F
+class(booleanFalse)
+
+string = "Hello"
+class(string)
+
+# math operations
+c = 2
+b = 4
+
+(c + b) * c
+e = (c + b) * c
+e
+
+# logical tests
+a = 1
+b = 2
+a > b
+a == b
+
+sqrt(2500)
