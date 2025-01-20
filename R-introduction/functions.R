5b33740d86b3740192267b4d3f2ec03e68b166a7
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2025-01-20 15:23:27 -0300

    Main Functions

--- R-introduction/functions.R
+++ R-introduction/functions.R
@@ -6,3 +6,24 @@ head(iris)
 head(iris, 2)
 head(n = 22)
 # ---
+
+# Main functions
+esoph
+head(esoph)
+tail(esoph)
+head(esoph, n = 10)
+
+dim(esoph)
+dim(esoph)[1]
+
+length(islands)
+
+colnames(esoph)
+
+rownames(esoph)
+
+summary(esoph)
+
+x = file.choose()
+x
+
