13a4e99a7698e973a12bd3263a22293a6096ed99
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2025-01-22 14:55:55 -0300

    finish activity

--- R-introduction/doitypurself.R
+++ R-introduction/doitypurself.R
@@ -19,6 +19,30 @@ for (i in v) {
   }
 }
 
-#quest 4
+# quest 4
 women
 tail(women, 10)
+
+#quest 5
+dataset1 <- iris['Sepal.Length'][50:100, ]
+dataset2 <- iris['Petal.Length'][50:100, ]
+
+plot(dataset1, dataset2,
+     main = 'activity 5',
+     xlab = 'sepal length',
+     ylab = 'pedal length',
+     col = 'blue', pch = 19)
+
+# quest 6
+v1 = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
+v2 = c(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L)
+
+v3 = v1 + v2
+print(v3)
+
+# quest 7
+CO2
+
+colnames(CO2)
+rownames(CO2)
+dim(CO2)
